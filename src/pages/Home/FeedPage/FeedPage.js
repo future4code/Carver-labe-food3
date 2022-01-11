54723074d461e93e5b0dbd0fd4c2bf601e455368
Juliana-Frossard <ju.frossard@hotmail.com>
2022-01-11 11:19:12 -0300

    alteraçoes na versao do material e react

--- src/pages/Home/FeedPage/FeedPage.js
+++ src/pages/Home/FeedPage/FeedPage.js
@@ -14,8 +14,7 @@ export const FeedPage = () =>{
       autoComplete="off"
     >
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      
     </Box>
    </div>
   )
